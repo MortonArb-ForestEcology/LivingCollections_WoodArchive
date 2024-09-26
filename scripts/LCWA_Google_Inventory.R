d189426e954c7316eb45cea1b63ac5ac1c522e79
Christy Rollinson <crollinson@gmail.com>
2024-09-26 09:46:30 -0500

    starter script for summarizing inventory spreadsheet

--- scripts/LCWA_Google_Inventory.R
+++ scripts/LCWA_Google_Inventory.R
@@ -1,15 +1,17 @@
 # Checking our Google Sheet Inventory and doing some quick summary stats
 
-lwca <- googlesheets::gs_title("LivingCollectionsArchive_Sample_Database")
-dat.samps <- data.frame(googlesheets::gs_read(lwca, ws="LCWA Inventory"))
-dat.samps$Year_collected <- as.numeric(substr(dat.samps$Date_collected, 1, 4))
+# Getting the key code for the spreadsheet called "LivingCollectionsArchive_SampleDatabase" 
+# This can be found in the url for a spreadsheet after the part that is [...]/spreadsheets/d and before the part that is /edit?[...]
+lwcaSS <- "1iVB5--m29mLKhXUNgmc92wijQDg98L6FoKvsrJBDkRg" 
+
+dat.samps <- data.frame(googlesheets4::read_sheet(ss=lwcaSS, sheet="LCWA Inventory"))
 dat.samps$SppName <- paste(dat.samps$Genus, dat.samps$Species)
 summary(dat.samps)
-
-length(unique(dat.samps$PlantID))
-length(unique(dat.samps$Genus))
-length(unique(dat.samps$SppName))
-
-spp.ID
-
 dim(dat.samps)
+
+dat.samps$Pith.Date <- as.numeric(dat.samps$Pith.Date)
+dat.samps$Earliest.Ring <- as.numeric(dat.samps$Earliest.Ring)
+dat.samps$Latest.Ring <- as.numeric(dat.samps$Latest.Ring)
+summary(dat.samps)
+
+
